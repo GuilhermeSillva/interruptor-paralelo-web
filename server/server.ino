4cdee124a97d3ea34a0037e9d8ddc26f7c8c7ba3
Guilherme Silva <guilhermesillva.grs@gmail.com>
2022-07-31 22:19:05 -0300

    :zap: envia html em pacotes

--- server/server.ino
+++ server/server.ino
@@ -10,9 +10,12 @@ int status = WL_IDLE_STATUS;
 
 WiFiEspServer server(80);
 
-RingBuffer buf(8);
+String http_req;
 
 File webFile;
+
+byte clientBuf[64];
+int clientCount = 0;
 
 void setup()
 {
@@ -37,25 +40,43 @@ void loop()
 {
   WiFiEspClient client = server.available();
   if (client){
+    boolean isCurrentLineBlank = true;
     while (client.connected()){
       if (client.available()){
         char request = client.read();
-        buf.push(request);
-        webFile = SD.open("index.htm");
-        if(buf.endsWith("\r\n\r\n")) {
+        if(http_req.length() < 80){
+          http_req += request;
+        }
+        if(request == '\n' && isCurrentLineBlank){
           client.println("HTTP/1.1 200 OK");
           client.println("Content-Type: text/html");
           client.println("Connection: close");
           client.println();
-          if (webFile){
-            while (webFile.available()){
-              client.write(webFile.read());
+
+          webFile = SD.open("index.htm");
+            if (webFile) {
+              while (webFile.available()) {
+                clientBuf[clientCount] = webFile.read();
+                clientCount++;
+                if(clientCount > 63){
+                  client.write(clientBuf, 64);
+                  clientCount = 0;
+                }
+              }
+              if(clientCount > 0) client.write(clientBuf, clientCount);
+              webFile.close();
             }
+
+          if (request == '\n') {
+            isCurrentLineBlank = true;
+          } else if (request != '\r') {
+            isCurrentLineBlank = false;
           }
-          break;
         }
       }
     }
+    delay(1);
     client.stop();
+    http_req = "";
   }
 }
