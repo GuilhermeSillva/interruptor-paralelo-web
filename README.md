fd8a825805746fa86928e0a8d5e485a029ebbfb6
Guilherme Silva <guilhermesillva.grs@gmail.com>
2022-08-09 14:04:28 -0300

    :recycle: troca arduino por wemos e passa a utilizar firebase

--- README.md
+++ README.md
@@ -3,10 +3,8 @@ Só quero apagar a luz do quarto sem ter que levantar da cama.
 
 
 ### Sobre
-A ideia é acessar uma página web que possibilite a interação com um interruptor paralelo, para isso é utilizado:
+A ideia é acessar uma página web que fará requisições para o "Realtime Database" da google. A placa Wemos estará lendo os dados do RTD e fará o controle do relé a partir dos dados coletados no banco e escreverá os dados coletados pelo leitor de tensão.
 
- - Arduino Uno R3, que será o cérebro da aplicação;
- - Um Shield WiFi ESP8266, que conectará o arduino a internet tornando-o um servidor web;
+ - Wemos D1 R2, que será o cérebro da aplicação;
  - Um módulo relé (5v), que atuará como um interruptor paralelo;
- - Um módulo leitor de micro SD, onde será armazenado os arquivos web;
  - Um módulo leitor de tensão, para retornar o estado atual da lâmpada, uma vez que pode-se acender ou apagar a mesma através de um interruptor que não tem ligação com o arduino.
--- server/server.ino
+++ server/server.ino
@@ -1,82 +1,16 @@
-#include "WiFiEsp.h"
-#include "env.h"
-#include <SPI.h>
-#include <SD.h>
+#include <ESP8266WiFi.h>
+#include <Firebase_ESP_Client.h>
+#include "addons/TokenHelper.h"
+#include "addons/RTDBHelper.h"
 
-char ssid[] = SSID;
-char pass[] = PASSWORD;
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
 
-int status = WL_IDLE_STATUS;
+void setup(){
 
-WiFiEspServer server(80);
-
-String http_req;
-
-File webFile;
-
-byte clientBuf[64];
-int clientCount = 0;
-
-void setup()
-{
-  Serial.begin(9600);
-  WiFi.init(&Serial);
-  WiFi.config(IPAddress(SERVERIP));
-
-  if (WiFi.status() == WL_NO_SHIELD){
-    while (true);
-  }
-  while (status != WL_CONNECTED){
-    status = WiFi.begin(ssid, pass);
-  }
-  server.begin();
-
-  if (!SD.begin(10)){
-    return;
-  }
 }
 
-void loop()
-{
-  WiFiEspClient client = server.available();
-  if (client){
-    boolean isCurrentLineBlank = true;
-    while (client.connected()){
-      if (client.available()){
-        char request = client.read();
-        if(http_req.length() < 80){
-          http_req += request;
-        }
-        if(request == '\n' && isCurrentLineBlank){
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println("Connection: close");
-          client.println();
+void loop(){
 
-          webFile = SD.open("index.htm");
-            if (webFile) {
-              while (webFile.available()) {
-                clientBuf[clientCount] = webFile.read();
-                clientCount++;
-                if(clientCount > 63){
-                  client.write(clientBuf, 64);
-                  clientCount = 0;
-                }
-              }
-              if(clientCount > 0) client.write(clientBuf, clientCount);
-              webFile.close();
-            }
-
-          if (request == '\n') {
-            isCurrentLineBlank = true;
-          } else if (request != '\r') {
-            isCurrentLineBlank = false;
-          }
-        }
-      }
-    }
-    delay(1);
-    client.stop();
-    http_req = "";
-  }
 }
